f9739e72ab5ea714a956c67fc41d0dade0adf597
陈虎 <chenhu2@xiaohongshu.net>
2022-01-01 00:27:38 +0800

    1:适配springboot2.6.X版本，解决循环依赖问题

---
+++ rpc-server3/src/main/java/com/example/rpcserver3/RpcServer3Application.java
@@ -0,0 +1,13 @@
+package com.example.rpcserver3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcServer3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcServer3Application.class, args);
+    }
+
+}
